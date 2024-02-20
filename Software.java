3b599399e0d5a6185a62deab592cb5bd0e2a8364
irosinsk-pnw <irosinsk@pnw.edu>
2024-02-20 17:32:01 -0600

    Add a time-wasting function.

--- Software.java
+++ Software.java
@@ -5,4 +5,22 @@ public class Software{
 
 
   }
+
+  /**
+   * Wastes the user's time and prints a friendly message.
+   *
+   * @param seconds The number of seconds to waste.
+   * @author irosinsk-pnw
+   */
+  public static void waste_time(int seconds) throws InterruptedException
+  {
+    System.out.println("Thank you for choosing to waste your time with me.");
+    for (int i = 0; i < seconds; i++)
+    {
+      Thread.sleep(500);
+      System.out.print(".");
+      Thread.sleep(500);
+    }
+    System.out.println("\nI hope this has been a good waste of time.");
+  }
 }
