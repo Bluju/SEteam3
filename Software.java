d592a6ccef0af732065a7d9ab1d9eb48804d79c8
msonn24 <159347995+msonn24@users.noreply.github.com>
2024-02-22 15:43:17 -0600

    Update Software.java
    added java function

--- Software.java
+++ Software.java
@@ -2,7 +2,12 @@ public class Software{
 
 
   public static void main(String[] args){
+		mattFunc();
 
+  }
+  
+  public static void mattFunc(){
+      System.out.println("Good afternoon.");
 
   }
 }
