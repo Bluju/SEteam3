82dfb6bfda4e35e5c8a712b1cd0307b46d440f28
ejvogt5 <vogte@pnw.edu>
2024-02-22 15:47:58 -0600

    call my func

--- Software.java
+++ Software.java
@@ -1,12 +1,12 @@
 public class Software{
 
-
-  public static void main(String[] args){
-
-
-  }
-
   public static void ethanFunc() {
     System.out.println( "Called Ethan's function!" )
   }
+
+  public static void main(String[] args){
+    ethanFunc();
+
+  }
+
 }
