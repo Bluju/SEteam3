95333936153aa36e2c9e8bd7541bb4de23d751ee
mvanmill13 <131725402+mvanmill13@users.noreply.github.com>
2024-02-22 14:16:18 -0600

    Update Software.java

--- Software.java
+++ Software.java
@@ -1,8 +1,25 @@
 public class Software{
 
 
-  public static void main(String[] args){
-
-
+public static void main(String[] args) 
+  {
+        System.out.println(generateGreeting());
   }
+   
+  // Function to generate a greeting based on the time of day
+    public static String generateGreeting() 
+  {
+        // Get the current hour
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+        int hour = calendar.get(java.util.Calendar.HOUR_OF_DAY);
+
+        // Determine the time of day and generate a greeting
+        if (hour < 12) {
+            return "Good morning!";
+        } else if (hour < 18) {
+            return "Good afternoon!";
+        } else {
+            return "Good evening!";
+        }
+   }
 }
