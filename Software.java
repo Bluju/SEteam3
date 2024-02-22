a0c75888b096408cdb98456e7cfc494d39df35bc
saraht314 <156364000+saraht314@users.noreply.github.com>
2024-02-22 15:47:02 -0600

    Update Software.java

--- Software.java
+++ Software.java
@@ -2,7 +2,13 @@ public class Software{
 
 
   public static void main(String[] args){
+    
 
 
   }
+  public static void sarahFunction()
+  {
+    System.out.print("This is Sarah's function"); 
+  }
+    
 }
