20270910da621295092d246e1b179fb068681685
ejvogt5 <vogte@pnw.edu>
2024-02-22 15:55:05 -0600

    fixed missing semicolon

--- Software.java
+++ Software.java
@@ -1,7 +1,7 @@
 public class Software{
 
   public static void ethanFunc() {
-    System.out.println( "Called Ethan's function!" )
+    System.out.println( "Called Ethan's function!" );
   }
 
   public static void main(String[] args){
