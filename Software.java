b7be1f2f6a912d8eb775d5e042afeeb5eac7ed11
Morgan Freeman <morkmail7@gmail.com>
2024-02-22 15:40:38 -0600

    mark branch

--- Software.java
+++ Software.java
@@ -1,8 +1,19 @@
-public class Software{
+public class Software {
+    
+  public static void main (String[] args) {
+      hello();
+  }
+      public static void hello() {
+      try {
 
+      System.out.println("Don't Believe Me? Here I'll put you on:");
+      Thread.sleep(1000);
+      System.out.println("Hellooo!");
+      Thread.sleep(1000);
+      System.out.println("That's you! That's how dumb you sound.");
+      }
+      catch (InterruptedException e) {
 
-  public static void main(String[] args){
-
-
+      }
   }
 }
