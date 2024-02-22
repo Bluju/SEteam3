6ace2d3c6f0771b48facd80721595acef1ba0b49
wesley1234897 <wroe.pnw.edu>
2024-02-22 14:42:24 -0600

    added wesley function

--- Software.java
+++ Software.java
@@ -2,7 +2,10 @@ public class Software{
 
 
   public static void main(String[] args){
+    wesleyFunc();
 
-
+  }
+  public static void wesleyFunc(){
+    System.out.println("This is Wesley's Functions!");
   }
 }
