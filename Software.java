66f4f094be766a4501cda0e8012fe1c7b0948926
Joe Udani <94575509+joeudani@users.noreply.github.com>
2024-02-22 14:47:14 -0600

    Update Software.java

--- Software.java
+++ Software.java
@@ -1,8 +1,12 @@
-public class Software{
+public class JoeFunction {
+    // prints the cubes of the ints from 1 up to and including n
+    public static void cubes(int n){
+        for(int i=1;i<=n;i++){
+            System.out.println((int)Math.pow(i,3));
+        }
 
-
-  public static void main(String[] args){
-
-
-  }
+    }
+    public static void main(String[] args) {
+        cubes(5);
+    }
 }
